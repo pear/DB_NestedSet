0c9196860947f398b0a31a96f4a451a6e5df29c4
Daniel Khan <datenpunk@php.net>
2003-10-06 23:41:25 +0000

    Added short howto on how to move to the 1.3+ db sheme
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_NestedSet/trunk@141954 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/mysql.sql
+++ docs/mysql.sql
@@ -1,19 +1,21 @@
 # The table which holds the structure
 CREATE TABLE tb_nodes (
- STRID int(11) NOT NULL auto_increment,
- ROOTID int(11) NOT NULL default '0',
- l int(11) NOT NULL default '0',
- r int(11) NOT NULL default '0',
- STREH tinyint(4) NOT NULL default '0',
- LEVEL int(11) NOT NULL default '0',
- STRNA char(128) NOT NULL default '',
- PRIMARY KEY (STRID),
- KEY ROOTID (ROOTID),
- KEY STREH (STREH),
- KEY l (l),
- KEY r (r),
- KEY SRLR (STRID,ROOTID,l,r),
- KEY LEVEL (LEVEL)
+  STRID int(11) NOT NULL auto_increment,
+  ROOTID int(11) NOT NULL default '0',
+  l int(11) NOT NULL default '0',
+  r int(11) NOT NULL default '0',
+  PARENT int(11) NOT NULL default '0',
+  STREH int(11) NOT NULL default '0',
+  LEVEL int(11) NOT NULL default '0',
+  STRNA char(128) NOT NULL default '',
+  PRIMARY KEY  (STRID),
+  KEY ROOTID (ROOTID),
+  KEY STREH (STREH),
+  KEY l (l),
+  KEY r (r),
+  KEY LEVEL (LEVEL),
+  KEY SRLR (ROOTID,l,r),
+  KEY parent (PARENT)
 ) TYPE=MyISAM COMMENT='NestedSet table';
 
 # A table which is used for a little table locking to avoid conflicts
