b51de46b0b37bd93edd618fc940b6d0b059c0a3d
Daniel Khan <datenpunk@php.net>
2003-11-21 21:24:01 +0000

    Added PATH_SEPARATOR constant for setting the include_path. Works now on windows too.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_NestedSet/trunk@144711 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/index.php
+++ tests/index.php
@@ -1,26 +1,50 @@
 <?php
+
 /**
+
 * UnitTest
+
 * Unit test interface for DB_NestedSet
+
 *
+
 * @author       Daniel Khan <dk@webcluster.at>
+
 * @package      DB_NestedSet
+
 * @version      $Revision$
+
 * @access       public
+
 */
-ini_set('include_path',realpath(dirname(__FILE__).'/../').':'.ini_get('include_path'));
+
+
+ini_set('include_path',realpath(dirname(__FILE__).'/../').PATH_SEPARATOR.ini_get('include_path'));
 
 
 require_once 'PHPUnit.php';
+
 require_once 'TestBase.php';
+
 require_once 'PHPUnit/GUI/HTML.php';
+
 require_once 'NestedSet.php';
+
 require_once 'UnitTest.php';
+
 ini_set('error_reporting',E_ALL);
+
+
 
 require_once 'PHPUnit/GUI/SetupDecorator.php';
 
+
+
 $gui = new PHPUnit_GUI_SetupDecorator(new PHPUnit_GUI_HTML());
+
 $gui->getSuitesFromDir(dirname(__FILE__),'.*[^_]\.php$',array('UnitTest.php','index.php','clitest.php','TestBase.php'));
+
 $gui->show();
+
 ?>
+
