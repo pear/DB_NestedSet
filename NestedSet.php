2ee6841028bee1556b82db8e68e6bd5780167457
Daniel Khan <datenpunk@php.net>
2007-10-21 23:30:54 +0000

    Preparing for 1.4.0RC1 - package2.xml
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_NestedSet/trunk@244559 c90b9560-bf6c-de11-be94-00142212c4b1

--- NestedSet.php
+++ NestedSet.php
@@ -1,28 +1,31 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PEAR :: DB_NestedSet                                                 |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Daniel Khan <dk@webcluster.at>                              |
-// |          Jason Rust  <jason@rustyparts.com>                          |
-// +----------------------------------------------------------------------+
-// $Id$
-// CREDITS:
-// --------
-// - Thanks to Kristian Koehntopp for publishing an explanation of the Nested Set
-// technique and for the great work he did and does for the php community
-// - Thanks to Daniel T. Gorski for his great tutorial on www.develnet.org
-// - Thanks to my parents for ... just kidding :]
+/**
+ +----------------------------------------------------------------------+
+ | PEAR :: DB_NestedSet                                                 |
+ +----------------------------------------------------------------------+
+ | Copyright (c) 1997-2003 The PHP Group                                |
+ +----------------------------------------------------------------------+
+ | This source file is subject to version 2.0 of the PHP license,       |
+ | that is bundled with this package in the file LICENSE, and is        |
+ | available at through the world-wide-web at                           |
+ | http://www.php.net/license/2_02.txt.                                 |
+ | If you did not receive a copy of the PHP license and are unable to   |
+ | obtain it through the world-wide-web, please send a note to          |
+ | license@php.net so we can mail you a copy immediately.               |
+ +----------------------------------------------------------------------+
+ | Authors: Daniel Khan <dk@webcluster.at>                              |
+ |          Jason Rust  <jason@rustyparts.com>                          |
+ +----------------------------------------------------------------------+
+ $Id$
+ CREDITS:
+ --------
+ - Thanks to Kristian Koehntopp for publishing an explanation of the Nested Set
+ technique and for the great work he did and does for the php community
+ - Thanks to Daniel T. Gorski for his great tutorial on www.develnet.org
+ - Thanks to my parents for ... just kidding :]
+*/
 require_once 'PEAR.php';
+
 // {{{ constants
 
 // Error and message codes
