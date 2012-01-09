7ca9c9d7133d05b5dacc62749314e35c72d94ab4
Daniel Convissor <danielc@php.net>
2012-01-09 22:10:21 +0000

    Set DSN from environment variables.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_NestedSet/trunk@321970 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/DB/NestedSet/TestCase.php
+++ tests/DB/NestedSet/TestCase.php
@@ -39,9 +39,67 @@ class DB_NestedSet_TestCase extends PHPUnit_Framework_TestCase  {
                 "tkey"           =>      "tkey"     // Custom - specify as many fields you want
         );
 
+        if (!empty($_ENV['MYSQL_TEST_USER']) && extension_loaded('mysqli')) {
+            $dsn = array(
+                'phptype' => 'mysqli',
+                'username' => $_ENV['MYSQL_TEST_USER'],
+                'password' => $_ENV['MYSQL_TEST_PASSWD'],
+                'database' => $_ENV['MYSQL_TEST_DB'],
+
+                'hostspec' => empty($_ENV['MYSQL_TEST_HOST'])
+                        ? null : $_ENV['MYSQL_TEST_HOST'],
+
+                'port' => empty($_ENV['MYSQL_TEST_PORT'])
+                        ? null : $_ENV['MYSQL_TEST_PORT'],
+
+                'socket' => empty($_ENV['MYSQL_TEST_SOCKET'])
+                        ? null : $_ENV['MYSQL_TEST_SOCKET'],
+            );
+        } elseif (!empty($_ENV['PGSQL_TEST_USER']) && extension_loaded('pgsql')) {
+            $dsn = array(
+                'phptype' => 'pgsql',
+                'username' => $_ENV['PGSQL_TEST_USER'],
+                'password' => $_ENV['PGSQL_TEST_PASSWD'],
+                'database' => $_ENV['PGSQL_TEST_DB'],
+
+                'hostspec' => empty($_ENV['PGSQL_TEST_HOST'])
+                        ? null : $_ENV['PGSQL_TEST_HOST'],
+
+                'port' => empty($_ENV['PGSQL_TEST_PORT'])
+                        ? null : $_ENV['PGSQL_TEST_PORT'],
+
+                'socket' => empty($_ENV['PGSQL_TEST_SOCKET'])
+                        ? null : $_ENV['PGSQL_TEST_SOCKET'],
+
+                'protocol' => empty($_ENV['PGSQL_TEST_PROTOCOL'])
+                        ? null : $_ENV['PGSQL_TEST_PROTOCOL'],
+
+                'option' => empty($_ENV['PGSQL_TEST_OPTIONS'])
+                        ? null : $_ENV['PGSQL_TEST_OPTIONS'],
+
+                'tty' => empty($_ENV['PGSQL_TEST_TTY'])
+                        ? null : $_ENV['PGSQL_TEST_TTY'],
+
+                'connect_timeout' => empty($_ENV['PGSQL_TEST_CONNECT_TIMEOUT'])
+                        ? null : $_ENV['PGSQL_TEST_CONNECT_TIMEOUT'],
+
+                'sslmode' => empty($_ENV['PGSQL_TEST_SSL_MODE'])
+                        ? null : $_ENV['PGSQL_TEST_SSL_MODE'],
+
+                'service' => empty($_ENV['PGSQL_TEST_SERVICE'])
+                        ? null : $_ENV['PGSQL_TEST_SERVICE'],
+            );
+        } else {
+            $this->markTestSkipped('DSN information not provided');
+        }
+
         $db_driver = 'DB';
-        $db_dsn    = 'mysql://localhost/test';
-        $this->_NeSe = DB_NestedSet::factory($db_driver, $db_dsn, $params);
+
+        $this->_NeSe = DB_NestedSet::factory($db_driver, $dsn, $params);
+        if (PEAR::isError($this->_NeSe)) {
+            $this->markTestSkipped($this->_NeSe->getMessage());
+        }
+
         $this->_NeSe->setAttr(array
         (
         'node_table' => 'tb_nodes',
